ae2b586246287abed15fc0282d4209e168e70e0a
putrapetirr0 <142271470+putrapetirr0@users.noreply.github.com>
2025-01-05 15:15:02 +0700

    ssh-vpn2.sh

--- ssh-vpn2.sh
+++ ssh-vpn2.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
-# =========================================
-# Quick Setup | Script Setup Manager
-# Edition : Stable Edition V1.0
-# Auther  : NevermoreSSH
-# (C) Copyright 2022
-# =========================================
 red='\e[1;31m'
 green='\e[0;32m'
 purple='\e[0;35m'
 orange='\e[0;33m'
 NC='\e[0m'
-export Server_URL="raw.githubusercontent.com/NevermoreSSH/yourpath/main"
+export Server_URL="raw.githubusercontent.com/putrapetirr0/tuman/refs/heads/main"
 clear
 dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
 biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
